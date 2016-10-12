b0e363a42e4c907cf860e38a245ad22ed975daf9
CameronC17 <cameronchalmers@hotmail.co.uk>
2016-10-12 13:26:52 +0100

    setting up the menu

--- client/js/game.js
+++ client/js/game.js
@@ -7,6 +7,12 @@ var ctx = c.getContext("2d");
 var loaded = false;
 var gameTick = 50;
 
+var numStars = 250;
+
+var gameDimensions = [3000, 3000];
+
+var screen = null;
+
 var test = false;
 
 //GAME DATA
@@ -27,15 +33,16 @@ var images = [
 ];
 
 var spriter = new Spriter();
-spriter.loadSprites(images);
+//spriter.loadSprites(images);
 
 function mainLoop() {
   clearScreen();
   if (!loaded) {
-    drawBackground();
+    drawLoading();
     checkLoad();
   } else {
-    drawSprites();
+    screen.draw();
+    //drawSprites();
   }
 }
 
@@ -44,7 +51,7 @@ function clearScreen() {
   ctx.fillRect(0, 0, c.width, c.height);
 }
 
-function drawBackground() {
+function drawLoading() {
   ctx.fillStyle="#23f390";
   ctx.fillRect(0, 0, c.width, c.height);
 }
@@ -53,6 +60,7 @@ function checkLoad() {
   ctx.fillStyle="#ff0000";
   if (spriter.checkLoaded()) {
     loaded = true;
+    screen = new Screen();
   }
   //var loadText = "LOADING";
   ctx.font="70px Arial";
@@ -70,7 +78,6 @@ function drawSprites() {
 }
 
 
-
 //This loops the animation frames for animation!!!!
 var recursiveAnim = function() {
           mainLoop();
@@ -86,3 +93,94 @@ function game() {
   }, gameTick);
 }
 game();
+
+
+// GAME CLASSES!!!!!!!!!!!!!!!
+
+class Screen {
+  constructor() {
+    //1=home 2=character creation 3=game,space 4=game,planet
+    this.screenPosition = 1;
+    this.stars = [];
+    this.createStars(numStars);
+  }
+
+  draw() {
+    switch (this.screenPosition) {
+      case 1:
+        this.drawMenu();
+        break;
+      case 2:
+        break;
+      default:
+        break;
+    }
+  }
+
+  drawMenu() {
+    this.drawMenuBackground();
+    this.drawStars();
+    this.drawTitle();
+  }
+
+  drawMenuBackground() {
+    ctx.fillStyle="#000";
+    ctx.fillRect(0, 0, c.width, c.height);
+  }
+
+  createStars(num) {
+    for (var i = 0; i < num; i++) {
+      var star = new Star(null, [c.width, c.height]);
+      star.speed = Math.floor(Math.random() * 3) + 1;
+      this.stars.push(star);
+    }
+  }
+
+  drawStars() {
+    ctx.fillStyle="#fff";
+    for (var i = 0; i < this.stars.length; i++) {
+      //draw star
+      var star = this.stars[i];
+      ctx.fillRect(star.x, star.y, star.size, star.size);
+
+      //move star
+      star.y += star.speed;
+
+      //now check if the stars are out of bounds
+      if (star.y > c.height + 20) {
+        star.x = Math.floor(Math.random() * c.width) + 0;
+        star.y = Math.floor(Math.random() * 0) - 40;
+      }
+    }
+  }
+
+  drawTitle() {
+    ctx.fillStyle="#43e2f1";
+    ctx.font="100px Arial";
+    ctx.fillText("NO CAM'S SKY", (c.width/2) - 360, 200);
+  }
+
+
+
+}
+
+
+
+class Star {
+  constructor(grav, limits) {
+    this.pull = grav;
+    this.x = Math.floor(Math.random() * limits[0]) + 0;
+    this.y = Math.floor(Math.random() * limits[1]) - 40;
+    this.size = Math.floor(Math.random() * 3) + 1;
+    this.speed = null;
+  }
+}
+
+
+class Button {
+  constructor() {
+
+  }
+
+
+}
